a42f7919f15510f1e94e9ffeeca3ce38cfdcbcb8
zengjing <hhtczengjing@gmail.com>
2018-12-02 20:51:57 +0800

    完善接口的代码

--- lib/flutter_hybrid_router.dart
+++ lib/flutter_hybrid_router.dart
@@ -3,18 +3,14 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterHybridRouter {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_hybrid_router');
+  static const MethodChannel _channel = const MethodChannel('flutter_hybrid_router');
 
   Future<void> openURL({String url, List parameters}) async {
-    _channel.invokeMethod(
-        'openURL', {"url": url ?? "", "parameters": (parameters ?? {})});
+    _channel.invokeMethod('openURL', {"url": url ?? "", "parameters": (parameters ?? [])});
   }
 
-  Future<Map> objectForURL(
-      {String url, List parameters}) async {
-    Map result = await _channel.invokeMethod(
-        'objectForURL', {"url": url ?? "", "parameters": (parameters ?? {})});
+  Future<Map> objectForURL({String url, List parameters}) async {
+    Map result = await _channel.invokeMethod('objectForURL', {"url": url ?? "", "parameters": (parameters ?? [])});
     return result;
   }
 }
